3fef4d23c6eac65bbb8f9f171a10afbb2799b920
Ezeekie04 <128231650+Ezeekie04@users.noreply.github.com>
2023-11-06 21:37:58 +0700

    tes

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to scroll back to the top
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
